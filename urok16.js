c6cffd28a4c3aca598610fa9213ebccf447f6167
Pavel <Lion4ik22@gmail.com>
2021-07-30 20:50:31 +0300

    urok16

---
+++ urok16.js
@@ -0,0 +1,39 @@
+'use strict';
+
+let num = 20;
+
+// function showFirstMessage() {
+//     console.log('Hello World!');
+// }
+
+// showFirstMessage();
+
+function showFirstMessage(text) {
+    console.log(text);
+    num = 10;
+}
+
+showFirstMessage('Hello World!');
+console.log(num);
+
+function calc(a, b) {
+    return (a + b);
+}
+
+console.log(calc(4, 3));    
+console.log(calc(5, 6));
+console.log(calc(10, 3));
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function() {
+    console.log('Hello');
+};
+
+logger();
