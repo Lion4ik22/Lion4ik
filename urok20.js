f0f2dde2847cad1faa956e567be127acba85f0fe
Pavel <Lion4ik22@gmail.com>
2021-08-06 20:55:41 +0300

    urok21

--- urok20.js
+++ urok20.js
@@ -7,8 +7,18 @@ const options = {
     colors: {
         border: 'black', 
         bg: 'red'
+    },
+    makeTest: function() {
+        console.log('Test');
     }
 };
+
+options.makeTest();
+
+const {border, bg} = options.colors;
+console.log(border);
+
+// console.log(Object.keys(options).length);
 
 // console.log(options ['colors'] ['border']);
 
@@ -16,17 +26,18 @@ const options = {
 
 // console.log(options);
 
-let counter = 0;
+// let counter = 0;
 
-for (let key in options) {
-     if (typeof(options[key]) === 'object') {
-        for (let i in options[key]) {
-             console.log(`Свойство ${i} имеет значение ${options[key] [i]}`); 
-         }
-     }
-     else {
-        console.log(`Свойство ${key} имеет значение ${options[key]}`);
-         counter++;
-    }
-}
-console.log(counter);
+// for (let key in options) {
+//      if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//              console.log(`Свойство ${i} имеет значение ${options[key] [i]}`); 
+//          }
+//      }
+//      else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//          counter++;
+//     }
+// }
+// console.log(counter);
+
