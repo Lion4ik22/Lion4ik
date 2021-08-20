22f3b485354c518ef5e895499ecbbabd4deb6948
Pavel <Lion4ik22@gmail.com>
2021-08-20 20:25:56 +0300

    urok26

---
+++ urok26.js
@@ -0,0 +1,52 @@
+'use strict';
+
+// To string    
+
+// 1)
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+
+// 1.2) 
+console.log(typeof(5 + ''));
+
+const num = 5;
+
+console.log('https://vk.com/catalog/' + num);
+
+const fontSize = 26  + 'px';
+
+// To number
+
+// 2
+console.log(typeof(Number('4')));
+
+// 2.1)
+console.log(typeof(+ '5'));
+
+// 2.3
+console.log(typeof(parseInt('15px', 10)));
+
+
+let answ = +prompt('Hello', '');
+
+// 3.1) To boolean
+
+// 0, '', null, undefined, NaN;
+
+let switcher = null;
+
+if (switcher) {
+    console.log('Working...');
+}
+
+switcher = 1;
+
+if (switcher) {
+    console.log('Working...');
+}
+
+//3.2)
+console.log(typeof(Boolean('4')));
+
+//3.3)
+console.log(typeof(!!'44444'));
